4448a9e5ab879907b840e40818a08f917e465161
YEUNG <feilung20060129@gmail.com>
2026-02-09 18:33:48 +0800

    Initial local commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Fragment } from "react/jsx-runtime";
+
+import { useState } from "react";
 
 function App() {
+  // const listContent = list.map(item =>(
+  //   <Fragment key={item.id}>
+  //     <li>{item.name}</li>
+  //     <li>-----------</li>
+  //   </Fragment>
+  // ))
+  //const [content, setContent] = useState('Default Content inside the tag')
+  const [data, setData] = useState([
+      { id: 1, name: 'Wong' },
+      { id: 2, name: 'Wang' },
+      { id: 3, name: 'Zhang' },
+      { id: 4, name: 'Li' },
+      { id: 5, name: 'Liu' }
+    ])
+    const listData = data.map(item => (
+      <li key={item.id}>{item.name}</li>
+    ))
+    let id = 3
+  function handleClick() {
+    setData([
+      ...data,
+      { id: id++, name: 'Zhao' }
+    ])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>{listData}</ul>
+      <button onClick={handleClick}>Button</button>
+    </>
+  )
 }
 
 export default App;
